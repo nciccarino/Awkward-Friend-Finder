4db87a93d683b619843c9c549154cd67e943854b
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-29 23:09:13 -0400

    css stuff

--- client/assets/clientJavaScript/survey.js
+++ client/assets/clientJavaScript/survey.js
@@ -1,32 +1,48 @@
 $("#submit").on("click", function(event) {
 	event.preventDefault();
-	var userData = {
-				name: $("#name").val().trim(), 
-				photo: $("#link").val().trim(), 
-				score: [
-					$("#q1").val(), 
-					$("#q2").val(),
-					$("#q3").val(),
-					$("#q4").val(),
-					$("#q5").val(),
-					$("#q6").val(),
-					$("#q7").val(),
-					$("#q8").val(),
-					$("#q9").val(),
-					$("#q10").val()
-				]
-			} //end userData
+	if ($(".form-control").val().trim() !== "") {
+		var userData = {
+					name: $("#name").val().trim(), 
+					photo: $("#link").val().trim(), 
+					score: [
+						$("#q1").val(), 
+						$("#q2").val(),
+						$("#q3").val(),
+						$("#q4").val(),
+						$("#q5").val(),
+						$("#q6").val(),
+						$("#q7").val(),
+						$("#q8").val(),
+						$("#q9").val(),
+						$("#q10").val()
+					]
+				} //end userData
 
-			console.log(userData); 
+		console.log(userData); 
 
-			var currentURL = window.location.origin; 
+		var currentURL = window.location.origin; 
 
-			console.log(currentURL); 
+		console.log(currentURL); 
 
-			$.post(currentURL + "/api/friends", userData, function(data) {
-				$("#matchName").text(data.name); 
-				$("#matchPhoto").attr("src", data.photo); 
-				$("#modalResults").modal('toggle'); 
-			}); 
+		$.post(currentURL + "/api/friends", userData, function(data) {
+			$("#matchName").text(data.name); 
+			$("#matchPhoto").attr("src", data.photo); 
+			$("#modalResults").modal('toggle'); 
+			$("#name").val(""); 
+			$("link").val(""); 
+		}); 
+	}//if statement 
+	else {
+		alert("Missing required information!"); 
+		$("#prompt").css("display", "block");
+		return false; 
+	} 
 
 }); //on click
+
+// //return home
+// $("#close").on("click", function(event) {
+// 	$.get("/", function(req, res) {
+// 		location.replace(res); 
+// 	});
+// });
